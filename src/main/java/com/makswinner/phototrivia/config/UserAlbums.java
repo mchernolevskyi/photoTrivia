061dda3ed9be73dbe1700c2221c95b5b132e0185
Maksym Chernolevskyi <maksym.chernolevskyi@atlas-iac.com>
2020-11-05 22:42:51 +0200

    New 1.2 jar

--- src/main/java/com/makswinner/phototrivia/config/UserAlbums.java
+++
@@ -1,9 +0,0 @@
-package com.makswinner.phototrivia.config;
-
-import java.util.List;
-
-public class UserAlbums {
-    private String username;
-    private String password;
-    private List<String> albums;
-}
